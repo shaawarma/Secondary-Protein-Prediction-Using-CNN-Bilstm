4af1919b8b64152f20e4d643ba62e21d8a997d12
shaawarma <varunvassagudam@gmail.com>
2026-02-05 13:47:34 +0530

    data preprocessing done

--- train.py
+++ train.py
@@ -1,9 +1,50 @@
 from src.config import WINDOW_SIZE
+from src.data.loader import load_raw_data
+from src.data.datapreprocessing import reshape_protein, extract_pssm, extract_labels, create_windows
+from src.data.datapreprocessing import preprocess_dataset
 import os
 
-print(WINDOW_SIZE)
 
 
-print(os.getcwd())          # where Python thinks your project root is
-print(os.listdir())        # what folders it sees here
-print(os.path.exists("data/"))  # should be True
+
+data = load_raw_data("cullpdb+profile_5926_filtered.npy")
+
+
+###print("Full shape:", data.shape)
+##print("One protein shape:", data[0].shape)
+#print("One residue shape:", data[0][0].shape)
+
+#protein = data[0].reshape(700, 57)
+
+#print(protein.shape)
+#print(protein[0])
+
+
+
+# check slices
+#print("AA part:", protein[0][:21])
+#print("PSSM part:", protein[0][21:42])
+#print("Label part:", protein[0][49:57])
+
+protein = data[0]
+
+p2d = reshape_protein(protein)
+pssm = extract_pssm(p2d)
+labels = extract_labels(p2d)
+
+windows = create_windows(pssm)
+
+print("PSSM shape:", pssm.shape)
+print("Windows shape:", windows.shape)
+print("Labels shape:", labels.shape)
+
+X, Y = preprocess_dataset(data)
+
+print("Final X:", X.shape)
+print("Final Y:", Y.shape)
+
+
+
+
+
+
